42edb814ff1da0bd1132bcf55638141f86fea446
Russell Heilling <russell@linx.net>
2015-04-19 03:28:11 +0100

    Don't need to export the Count type so rename to count
    Be more consistent with receiver naming (use self in Counter methods
    to match NumberList methods)

--- go/counter/counter.go
+++ go/counter/counter.go
@@ -1,30 +1,30 @@
 package counter
 
-type Count struct {
+type count struct {
     Key string
     Count int
 }
 
-type Counter []Count
+type Counter []count
 
 func LoadMap(source map[string]int) *Counter {
     counts := make(Counter, len(source))
     i := 0
     for k, v := range source {
-        counts[i] = Count{Key: k, Count: v}
+        counts[i] = count{Key: k, Count: v}
         i++
     }
     return &counts
 }
 
-func (p Counter) Len() int {
-    return len(p)
+func (self Counter) Len() int {
+    return len(self)
 }
 
-func (p Counter) Less(i, j int) bool {
-    return p[i].Count < p[j].Count
+func (self Counter) Less(i, j int) bool {
+    return self[i].Count < self[j].Count
 }
 
-func (p Counter) Swap(i, j int) {
-    p[i], p[j] = p[j], p[i]
+func (self Counter) Swap(i, j int) {
+    self[i], self[j] = self[j], self[i]
 }
