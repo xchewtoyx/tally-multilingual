c08df26ee3740c1768cb4502626ba5b497cb27b3
Russell Heilling <russell@linx.net>
2015-04-19 03:17:55 +0100

    move go implementation to strings.Scanner and python implementation to defaultdict.
    factor of 2 speedup for python using defaultdict rather than Counter()

--- go/counter/numberlist.go
+++ go/counter/numberlist.go
@@ -12,7 +12,8 @@ type NumberList chan string
 
 func chomp(r rune) bool { return r == '\n' }
 
-func (self NumberList) ReadNumbers(buffer *bufio.Reader) {
+func (self NumberList) ReadLines(infile io.Reader) {
+    buffer := bufio.NewReader(infile)
     defer close(self)
     for {
         line, err := buffer.ReadString('\n')
@@ -23,12 +24,22 @@ func (self NumberList) ReadNumbers(buffer *bufio.Reader) {
     }
 }
 
+func (self NumberList) ScanLines(infile io.Reader) {
+    scanner := bufio.NewScanner(infile)
+    defer close(self)
+    for scanner.Scan() {
+        self <- scanner.Text()
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
+
 func (self NumberList) ReadNumbersFromFile(filename string) {
     infile, err := os.Open(filename)
     if err != nil {
         log.Fatal(err)
     }
     defer infile.Close()
-    buffer := bufio.NewReader(infile)
-    self.ReadNumbers(buffer)
+    self.ScanLines(infile)
 }
--- python/top5.py
+++ python/top5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from collections import Counter
+from collections import defaultdict
 import sys
 
 def readnumbers(filename):
@@ -8,18 +8,21 @@ def readnumbers(filename):
         for line in input:
             yield line.strip()
 
-def counts(filename):
-    counter = Counter()
+def tally_counts(filename):
+    counter = defaultdict(int)
     for number in readnumbers(filename):
         counter[number] += 1
-    return counter
+    for key, count in counter.items():
+        yield count, key
 
 def top5(filename):
-    return counts(filename).most_common(5)
+    counts = tally_counts(filename)
+    most_common = sorted(tally_counts(filename), reverse=True)
+    return most_common[:5]
 
 def main():
-    for pair in top5(sys.argv[1]):
-        print "%4s %s" % pair
+    for count, key in top5(sys.argv[1]):
+        print "%4s %s" % (key, count)
 
 if __name__ == '__main__':
     main()
